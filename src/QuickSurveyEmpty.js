6654a7b883d74a7c7d32147d867b50d2c59dcf66
yedeshamda <61509457+yedeshamda@users.noreply.github.com>
2024-01-31 13:30:01 +0100

    Push 90 Min
    last Push By Yedes Hamda

--- src/QuickSurveyEmpty.js
+++ src/QuickSurveyEmpty.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';  // Importez le composant de l'éditeur de texte enrichi
 import 'react-quill/dist/quill.snow.css';  // Importez les styles de l'éditeur de texte enrichi
 import countryCallingCode from 'country-calling-code'; // Importer le module pour les codes de pays
+import {Link, useParams} from 'react-router-dom';
 
 const QuickSurveyEmpty = () => {
+    const { iduser } = useParams();
 
     return (
         <div className="App1">
@@ -37,17 +39,48 @@ const QuickSurveyEmpty = () => {
             <div style={{width: 275, height: 141, left: 1164, top: 851, position: 'absolute', background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.50) 100%)', borderRadius: 16}} >
                 <img src={process.env.PUBLIC_URL + '/s8.png'} alt="LogoTale" style={{ width: '100%', height: '100%' }} />
             </div>
-    <div style={{width: 216, height: 54, left: 320, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Household Consumption<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 320, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Leisure and Interests<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 320, top: 1079, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Education<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 906, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Automobile<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 906, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Consumer Foods and Beverages<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 615, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Occupation<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 615, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Video Games and Computer Games<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 615, top: 1079, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Consumer health<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 1195, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Mass Tourism<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{width: 216, height: 54, left: 1195, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Multimedia<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span></div>
-    <div style={{paddingLeft: 8, paddingRight: 8, paddingTop: 4, paddingBottom: 4, left: 300, top: 702, position: 'absolute', background: '#00BDA9', borderRadius: 20, justifyContent: 'center', alignItems: 'center', gap: 4, display: 'inline-flex'}}>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 320, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Household Consumption<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+            </div>
+        </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+            <div style={{width: 216, height: 54, left: 320, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Leisure and Interests<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+            </div>
+            </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 320, top: 1079, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Education<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 906, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Automobile<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 906, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Consumer Foods and Beverages<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 615, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Occupation<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 615, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Video Games and Computer Games<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 615, top: 1079, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Consumer health<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 1195, top: 765, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Mass Tourism<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+            <Link to={`/surveydetails/${iduser}`}>
+                <div style={{width: 216, height: 54, left: 1195, top: 922, position: 'absolute', textAlign: 'right'}}><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '700', wordWrap: 'break-word'}}>Multimedia<br/></span><span style={{color: 'white', fontSize: 14, fontFamily: 'revert', fontWeight: '400', wordWrap: 'break-word'}}>(19 questions)</span>
+                </div>
+            </Link>
+            <div style={{paddingLeft: 8, paddingRight: 8, paddingTop: 4, paddingBottom: 4, left: 300, top: 702, position: 'absolute', background: '#00BDA9', borderRadius: 20, justifyContent: 'center', alignItems: 'center', gap: 4, display: 'inline-flex'}}>
         <div style={{textAlign: 'right', color: 'white', fontSize: 10, fontFamily: 'revert', fontWeight: '600', wordWrap: 'break-word'}}>10 points</div>
     </div>
     <div style={{paddingLeft: 8, paddingRight: 8, paddingTop: 4, paddingBottom: 4, left: 300, top: 859, position: 'absolute', background: '#00BDA9', borderRadius: 20, justifyContent: 'center', alignItems: 'center', gap: 4, display: 'inline-flex'}}>
