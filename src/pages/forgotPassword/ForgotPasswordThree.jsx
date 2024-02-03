834974fdd2c8760645f916a771b7140f53b9388b
Rayan Ghith <rayen.ghith@enicar.ucar.tn>
2024-02-03 16:30:16 +0100

    Login signup frontend final

--- src/pages/forgotPassword/ForgotPasswordThree.jsx
+++ src/pages/forgotPassword/ForgotPasswordThree.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react'
 import LeftBackground from '../../components/LeftBackground';
+import { useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
 
 function ForgotPasswordThree() {
 
+  const navigate = useNavigate();
+  
   const [password, setPassword] = useState('');
   const [strength, setStrength] = useState('');
   const [isChecked1, setIsChecked1] = useState(false);
   const [isChecked1password, setChecked1] = useState(false);
   const [isChecked2password, setChecked2] = useState(false);
   const [isChecked3password, setChecked3] = useState(false);
-
-
-
-
-  const [isPasswordVerified, setIsPasswordVerified] = useState(false);
-
-
   const [checked, setChecked] = React.useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleClick = () => {
     setChecked(!checked);
-    const isVerified = document.querySelector('input[type="password"]').value.trim() !== '';
-    setIsPasswordVerified(isVerified);
     const isWeak = !password.toLowerCase().includes('name') && !password.toLowerCase().includes('@' || '.com' || '.tn');
     const isGood = password.length >= 8;
     const isStrong = /[!@#$%^&*()_+\-={};':"\\|,.<>?0-9]/.test(password);
@@ -31,9 +31,13 @@ function ForgotPasswordThree() {
     setChecked2(isGood);
     setChecked3(isStrong);
 
-    console.log('Checkbox states:', isChecked1password, isChecked2password, isChecked3password);
-  };
 
+    console.log('Checkbox states:', isChecked1password, isChecked2password, isChecked3password);
+
+    if (isChecked1password && isChecked2password && isChecked3password) {
+      navigate('/login');
+    }
+  };
 
   const handleChange = (event) => {
     setChecked(event.target.checked);
@@ -42,9 +46,11 @@ function ForgotPasswordThree() {
     setIsChecked1(true);
   };
 
+
+
   const validatePassword = () => {
     const containsNameEmail = !password.toLowerCase().includes('name') && !password.toLowerCase().includes('@' || '.com' || '.tn');
-    const containsSymbolNumber = /[!@#$%^&*()_+\-{};':"\\|,.<>?0-9]/.test(password);
+    const containsSymbolNumber = /[!@#$%^&*()_+\-={};':"\\|,.<>?0-9]/.test(password);
     const hasValidLength = password.length >= 8;
 
     if (containsNameEmail && containsSymbolNumber && hasValidLength) {
@@ -56,8 +62,6 @@ function ForgotPasswordThree() {
     } else {
       setStrength('');
     }
-
-
   };
   // Function to determine color dynamically
   const getColor = () => {
@@ -73,123 +77,180 @@ function ForgotPasswordThree() {
     }
   };
 
+
+
   const handlePasswordSubmit = (e) => {
     e.preventDefault();
     console.log('Password:', password);
-
   };
 
+
+  
   const passwordStyle = {
-    padding: '100px',
-    paddingTop: '100px',
-    paddingLeft: '300px',
-    textAlign: 'center',
-    height: '700px',
+    height: '100%',
+    flex: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center', // Center items horizontally
     position: 'relative',
-  }
+    top: '120px',
+    left: '231px',
+  };
 
   return (
-
-    <div className="container" style={passwordStyle}>
+    <>
       <LeftBackground mainText={"Forget PWD?"} detailsText={"Don't worry :))"} />
-      <div>
-        <h1>Create Password</h1>
-      </div>
-      <div style={{ textAlign: 'Left', paddingTop: '30px', position: 'relative', left: '324px' }}>
-        <form onSubmit={handlePasswordSubmit}>
-          <div className="mb-3">
-            <label htmlFor="formGroupPassword" className="form-label" >
-              <strong>Password</strong>
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="formGroupPassword"
-              placeholder="Enter a password"
-              style={{ width: '400px !important', height: '51px', border: '2px solid #00C0FC', padding: '12px', transition: 'border-color 0.3s ease', borderColor: '#00C0FC' }}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                validatePassword();
-              }}
-              required />
-
-            <div style={{ paddingTop: '20px', display: 'flex', color: '#666666', fontSize: '16' }}>Password Strength: <div style={{ color: getColor(), paddingLeft: '15px' }}>{strength}</div></div>
-            {/**CHeck valid of Password */}
+      {/***create password interface***/}
+      <div className="container" style={passwordStyle}>
+        <div>
+          <h1>Create New Password</h1>
+        </div>
+        <div style={{ textAlign: 'Left', paddingTop: '30px', }}>
+          <form onSubmit={handlePasswordSubmit}>
             <div>
-              <div style={{ display: "flex" }}>
-                <div className="form-check">
-                  <input className="form-check-input" style={{
-                    marginTop: '5px',
-                    border: '2px solid', // Border color
-                    borderRadius: '14px', // Border radius
-                    marginBottom: '15px',
-                    height: '20px', // Height
-                    marginRight: '5px',// Optional: Add margin for better spacing,
-                    color: '#00BDA9',
-                    width: '24px',  backgroundColor: isChecked1 ? '#00BDA9' : '#ffffff',
+              <label htmlFor="formGroupPassword" className="form-label" >
+                <strong>
+                  Password</strong>
+              </label>
+              <div style={{ display: "flex", paddingTop: "15px", position: "relative" }}>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  className="form-control"
+                  id="formGroupPassword"
+                  placeholder="Enter a new password"
+                  style={{
+                    width: '400px', // Set the desired width
+                    height: '51px',
+                    border: '2px solid #00C0FC',
+                    padding: '12px',
+                    transition: 'border-color 0.3s ease',
+                    borderColor: '#00C0FC',
                   }}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    validatePassword();
+                  }}
+                />
+                <div style={{ position: 'absolute', right: '12px', top: '50%', transform: 'translateY(-50%)', paddingTop: '11px' }}>
+                  {/* Utilisez l'icône de l'oeil pour montrer/masquer le mot de passe */}
+                  {showPassword ? (
+                    <FaEyeSlash
+                      onClick={togglePasswordVisibility}
+                      style={{
+                        //position: 'relative',
+                        width: '19px',
+                        height: '19px',
+                        left: '660px',
+                        top: '120px',
+                        border: 'none',
+                        cursor: 'pointer',
+                        color: "#606060",
 
-                    onChange={() => handleCheckboxChangep1(isChecked1password)} checked={isChecked1password} type="checkbox" id="gridCheckp1" />
-                  <label className="form-check-label" htmlFor="gridCheckp1" style={{ paddingLeft: '12px', color: '#666666' }} >
-                    Must not contain your name or email
-                  </label>
+                      }}
+                    />
+
+                  ) : (
+                    <FaEye
+                      onClick={togglePasswordVisibility}
+                      style={{
+                        //position: 'absolute',
+                        width: '19px',
+                        height: '19px',
+                        left: '660px',
+                        top: '180px',
+                        border: 'none',
+                        cursor: 'pointer',
+                        color: "#606060",
+
+                      }}
+                    />
+
+                  )}
                 </div>
               </div>
-              <div style={{ display: "flex" }}>
-                <div className="form-check">
-                  <input className="form-check-input" style={{
-                    border: '2px solid', // Border color
-                    borderRadius: '14px', // Border radius
-                    marginBottom: '15px',
-                    height: '20px', // Height
-                    marginRight: '5px',// Optional: Add margin for better spacing,
-                    color: '#00BDA9',
-                    width: '24px',  backgroundColor: isChecked1 ? '#00BDA9' : '#ffffff',
-                  }}
-                    onChange={handleChange} checked={isChecked2password} type="checkbox" id="gridCheckp2" />
-                  <label className="form-check-label" htmlFor="gridCheckp2" style={{ paddingLeft: '12px', color: '#666666' }} required>
-                    At least 8 characters
-                  </label>
-                </div>
-              </div>
-              <div style={{ display: "flex", paddingBottom: '24px' }}>
-                <div className="form-check" >
-                  <input className="form-check-input" style={{
-                    border: '2px solid',
-                    borderRadius: '14px',
-                    height: '20px', // Height
-                    marginRight: '5px',// Optional: Add margin for better spacing,
-                    color: '#00BDA9',
-                    width: '24px',  backgroundColor: isChecked1 ? '#00BDA9' : '#ffffff',
-                  }}
-                    checked={isChecked3password} onChange={handleChange} type="checkbox" id="gridCheckp3" />
-                  <label className="form-check-label" htmlFor="gridCheckp3" style={{ paddingLeft: '12px', color: '#666666' }} required>
-                    Contains a symbol or a number
-                  </label>
-                </div>
-              </div>
 
 
-            </div>
-            {/**Button Reset Password */}
-            <div className="container" style={{ display: 'flex', padding: '24px!important' }}>
+              <div style={{ paddingTop: '20px', display: 'flex', color: '#666666', fontSize: '16' }}>Password Strength: <div style={{ color: getColor(), paddingLeft: '15px' }}>{strength}</div></div>
+              {/**CHeck valid of Password */}
+              <div>
+                <div style={{ display: "flex" }}>
+                  <div className="form-check">
+                    <input className="form-check-input" style={{
+                      marginTop: '5px',
+                      border: '2px solid',
+                      borderRadius: '14px',
+                      marginBottom: '15px',
+                      marginRight: '5px',
+                      width: '24px', height: '24px', backgroundColor: isChecked1password ? '#00BDA9' : '#ffffff',
+                      color: isChecked1password ? '#ffffff' : '#00BDA9',
+
+                    }}
+
+                      onChange={() => handleCheckboxChangep1(isChecked1password)} checked={isChecked1password} type="checkbox" id="gridCheckp1" />
+                    <label className="form-check-label" htmlFor="gridCheckp1" style={{ paddingLeft: '12px', color: '#666666' }} >
+                      Must not contain your name or email
+                    </label>
+                  </div>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <div className="form-check">
+                    <input className="form-check-input" style={{
+
+                      border: '2px solid', // Border color
+                      borderRadius: '14px', // Border radius
+                      marginBottom: '15px',
+                      marginRight: '5px',
+                      width: '24px', height: '24px', backgroundColor: isChecked2password ? '#00BDA9' : '#ffffff',
+                      color: isChecked2password ? '#ffffff' : '#00BDA9',
+                    }}
+                      onChange={handleChange} checked={isChecked2password} type="checkbox" id="gridCheckp2" />
+                    <label className="form-check-label" htmlFor="gridCheckp2" style={{ paddingLeft: '12px', color: '#666666' }} required>
+                      At least 8 characters
+                    </label>
+                  </div>
+                </div>
+                <div style={{ display: "flex", paddingBottom: '24px' }}>
+                  <div className="form-check" >
+                    <input className="form-check-input" style={{
+                      border: '2px solid',
+                      borderRadius: '14px',
+                      marginRight: '5px',
+                      width: '24px', height: '24px', backgroundColor: isChecked3password ? '#00BDA9' : '#ffffff',
+                      color: isChecked3password ? '#ffffff' : '#00BDA9',
+                    }}
+                      checked={isChecked3password} onChange={handleChange} type="checkbox" id="gridCheckp3" />
+                    <label className="form-check-label" htmlFor="gridCheckp3" style={{ paddingLeft: '12px', color: '#666666' }} required>
+
+                      Contains a symbol or a number
+                    </label>
+                  </div>
+                </div>
+
+              </div>
+              {/**Button reset password */}
+
+
               <button style={{
+                background: 'linear-gradient(90deg, #00BDA9 0%, #00C0FC 100%)',
                 width: '400px',
                 height: '51px',
-                top: ' 463px',
-                left: ' 649px',
-                borderRadius: '10px', background: 'linear-gradient(90deg, #00BDA9 0%, #00C0FC 100%)',
+                padding: '16px',
+                borderRadius: '10px',
+                color: 'white',
+                textAlign: 'center',
+                lineHeight: '19px',
+                fontFamily: 'TaleBlue, sans-serif',
+                fontSize: '14px',
                 cursor: 'pointer',
-                transition: 'background 300ms ease-out',
                 border: 'none',
-                color: '#FFFFFF',
-                justifyContent: 'center !important',
-              }} onClick={handleClick}>Reset Password</button>
+                outline: 'none',
+              }} onClick={handleClick}  >Reset Password</button>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div >
-    </div >
-  )
+    </>
+  );
 }
 export default ForgotPasswordThree
